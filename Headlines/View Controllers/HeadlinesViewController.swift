e59cdab7f75863cb9592b0e3281e314bb983ecb8
Ryan Ackermann <ryan@ackermann.io>
2019-09-02 19:23:49 +0200

    Added the base project files.

---
+++ Headlines/View Controllers/HeadlinesViewController.swift
@@ -0,0 +1,41 @@
+
+import UIKit
+
+class HeadlinesViewController: UITableViewController {
+    
+    private let cellIdentifier = "reuseIdentifier"
+
+    init() {
+        super.init(style: .insetGrouped)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        clearsSelectionOnViewWillAppear = true
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        
+
+        return cell
+    }
+
+}
